dcaf2d2e510762410520f2f8167f85cb344f3925
mengxinyu <442108590@qq.com>
2023-05-17 18:42:39 +0800

    add 股票问题

---
+++ src/Subject/股票/Code188.java
@@ -0,0 +1,105 @@
+package Subject.股票;
+
+/**
+ * @author mxy
+ * @create 2023-05-17 15:38
+ */
+
+/**
+ * 188. 买卖股票的最佳时机 IV    链接：https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iv
+ * 给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格，和一个整型 k 。
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。也就是说，你最多可以买 k 次，卖 k 次。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ *
+ * 示例 1：
+ * 输入：k = 2, prices = [2,4,1]
+ * 输出：2
+ * 解释：在第 1 天 (股票价格 = 2) 的时候买入，在第 2 天 (股票价格 = 4) 的时候卖出，这笔交易所能获得利润 = 4-2 = 2 。
+ *
+ * 示例 2：
+ * 输入：k = 2, prices = [3,2,6,5,0,3]
+ * 输出：7
+ * 解释：在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
+ *      随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 = 3 。
+ *
+ * 提示：
+ * 0 <= k <= 100
+ * 0 <= prices.length <= 1000
+ * 0 <= prices[i] <= 1000
+ *
+ */
+public class Code188 {
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,6,5,0,3};
+        System.out.println(maxProfit(2, nums));
+    }
+
+    public static int maxProfit(int k, int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        if (k >= prices.length/2) return maxProfit(prices);
+        int length = prices.length;
+        int[][][] dp = new int[length][k + 1][2];
+        for (int i = 1; i <= k; i++) {
+            dp[0][i][0] = 0;
+            dp[0][i][1] = -prices[0];
+        }
+        for (int i = 1; i < prices.length; i++) {
+            for (int j = k; j > 0; j--) {
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+            }
+        }
+        return dp[length - 1][k][0];
+    }
+
+    public static int maxProfit(int[] prices){
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int length = prices.length;
+        int[][] dp = new int[length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[length - 1][0];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
